9b844f644574145e0d22b58002d76ee04fafb478
MargoKrk <malgorzata.walega89@gmail.com>
2023-08-28 23:14:04 +0200

    active class in header

--- src/components/Header/NavItem.tsx
+++ src/components/Header/NavItem.tsx
@@ -10,8 +10,8 @@ export const NavItem = (props: NavItemProps) => {
 const {children, to, ...other} = props;
 
   return (
-    <li className="p-6 active:text-[#FCFE7F]" {...other}>
-      <NavLink to={to}>{children}</NavLink>
+    <li className="p-6" {...other}>
+      <NavLink to={to} className={({isActive}) => isActive ? "text-[#FCFE7F]" : "text-white" }>{children}</NavLink>
     </li>
   );
 };
