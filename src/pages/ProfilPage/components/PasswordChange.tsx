b8941d626d7ac0ba6375c8bb7299b816f5901155
MargoKrk <malgorzata.walega89@gmail.com>
2023-09-22 19:54:19 +0200

    changePassword form

--- src/pages/ProfilPage/components/PasswordChange.tsx
+++ src/pages/ProfilPage/components/PasswordChange.tsx
@@ -1,5 +1,21 @@
+import { TextField } from "../../../components";
+import { Button } from "../../../components/Button";
+
 export const PasswordChange = () => {
-    return (
-        <div>Zmiana hasła</div>
-    )
-}
+  return (
+    <>
+      <form className="grid w-[515px]">
+        <div className="grid grid-rows-3 gap-y-[34px]">
+          <TextField label="Stare hasło" required={true} />
+          <TextField label="Nowe hasło" required={true} />
+          <TextField label="Powtórz nowe hasło" required={true} />
+        </div>
+        <div className="flex justify-end mt-[42px]">
+          <Button variant="contained" type="submit">
+            Zmień hasło
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
